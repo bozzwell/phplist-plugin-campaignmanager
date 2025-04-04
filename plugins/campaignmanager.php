044928fc0e2e916206dbc855e572532576044c0c
bozzwell <55630215+bozzwell@users.noreply.github.com>
2025-04-04 15:10:46 +0200

    APi update

--- plugins/campaignmanager.php
+++ plugins/campaignmanager.php
@@ -19,7 +19,8 @@ class campaignmanager extends phplistPlugin
     
     // Oldalcímek beállítása
     public $pageTitles = array(
-        'main' => 'Campaign Manager'
+        'main' => 'Campaign Manager',
+        'settings' => 'API beállítások'
     );
     
     // Konstruktor
@@ -27,6 +28,17 @@ class campaignmanager extends phplistPlugin
     {
         // Kódgyökér beállítása
         $this->coderoot = dirname(__FILE__) . '/campaignmanager/';
+        
+        // API kérés kezelése
+        if (isset($_GET['page']) && $_GET['page'] == 'campaignmanager' && 
+            isset($_GET['pi']) && $_GET['pi'] == 'campaignmanager' &&
+            (isset($_GET['api']) || isset($_POST['api']))) {
+            
+            // Biztonságosan betöltjük az API fájlt
+            if (file_exists($this->coderoot . 'api.php')) {
+                include_once $this->coderoot . 'api.php';
+            }
+        }
         
         // Szülő konstruktor hívása
         parent::__construct();
@@ -36,7 +48,23 @@ class campaignmanager extends phplistPlugin
     public function adminmenu()
     {
         return array(
-            'main' => 'Campaign Manager'
+            'main' => 'Campaign Manager',
+            'settings' => 'API beállítások'
         );
+    }
+    
+    // API kulcs generálása, ha még nincs
+    public function activate()
+    {
+        parent::activate();
+        
+        // API kulcs generálása, ha még nincs
+        $api_key = $this->getPluginOption('api_key');
+        if (!$api_key) {
+            $api_key = md5(uniqid(rand(), true));
+            $this->setPluginOption('api_key', $api_key);
+        }
+        
+        return true;
     }
 }
